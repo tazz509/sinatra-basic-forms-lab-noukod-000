4169c24bea47b26f77583aaf9f1c08dcc3698b53
carly <carly@noukod.com>
2019-01-14 19:44:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/new' do
+    @name=params[:name]
+    @age=params[:age]
+    @breed=params[:breed]
+    erb :display_puppy
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
 
 end
