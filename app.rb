8caf7db77017789c7b553347ca06251fd27d1235
carly <carly@noukod.com>
2019-01-14 19:37:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-get '/' do
-  erb: index
-end
+  post '/new' do
+    @name=params[:name]
+    @age=params[:age]
+    @breed=params[:breed]
+    erb :display_puppy
+  end
 
-  post '/' do
-end
-
-get '/' do
-end
+  get '/new' do
+    erb :create_puppy
+  end
 
 end
